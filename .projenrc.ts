79168e4ac94e86cf15a7d1f7cd622f77d1ff3441
Andrew Hammond <445764+ahammond@users.noreply.github.com>
2025-10-31 17:44:51 -0700

    chore(projen): remove deprecated packageManager configuration

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,13 +1,9 @@
 import { clickupTs } from '@time-loop/clickup-projen';
-import { javascript } from 'projen';
 
 const project = new clickupTs.ClickUpTypeScriptProject({
   name: '@time-loop/cdk-log-parser',
   defaultReleaseBranch: 'main',
   licensed: true,
-  packageManager: javascript.NodePackageManager.PNPM,
-  pnpmVersion: '9',
-
   projenrcTs: true,
   bin: {
     'cdk-log-parser': 'src/cdkLogParser.ts',
